4a058aa87e828108e466f7ab1c62115cf293b7f3
Hussain101 <hussainsidd88@gmail.com>
2023-12-11 22:39:35 +0500

    users and product completed

--- app/ui/dashboard/footer/Footer.jsx
+++ app/ui/dashboard/footer/Footer.jsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
   return (
-    <div className={"flex justify-between text-slate-300"}>
+    <div className={"w-[90%] relative -right-10 flex justify-between text-slate-300"}>
       <div className={""}>HS-Code Solutions</div>
       <div className={""}>© All rights reserved.</div>
     </div>
